5accaac0b21a78967644c72d7ec8fee93c409bb9
John DeWyze <john.dewyze@shopify.com>
2021-07-21 08:34:08 -0500

    Run prettier

--- grammar.js
+++ grammar.js
@@ -1,123 +1,99 @@
 module.exports = grammar({
-  name: 'liquid',
+  name: "liquid",
 
-  precedences: $ => [
+  precedences: ($) => [
     [
-      'unary_not',
-      'binary_exp',
-      'binary_times',
-      'binary_plus',
-      'binary_in',
-      'binary_compare',
-      'binary_relation',
-      'clause_connective',
+      "unary_not",
+      "binary_exp",
+      "binary_times",
+      "binary_plus",
+      "binary_in",
+      "binary_compare",
+      "binary_relation",
+      "clause_connective",
     ],
   ],
 
   rules: {
-    program: $ => repeat1(
-      seq(
-        choice('{{', '{%'),
-        choice(
-          $.filter,
-          $.expression,
-          $.statement
-        ),
-        choice('}}', '%}'),
-      ),
-    ),
-
-    filter: $ => seq(
-      field("body", choice($.expression, $.filter)),
-      "|",
-      field('name', $.identifier),
-      optional(
+    program: ($) =>
+      repeat1(
         seq(
-          ":",
-          $.argument_list,
-        ),
+          choice("{{", "{%"),
+          choice($.filter, $.expression, $.statement),
+          choice("}}", "%}")
+        )
       ),
-    ),
 
-    statement: $ => choice(
-      $.assignment,
-    ),
-
-    expression: $ => choice(
-      $._literal,
-      $.identifier,
-      $.predicate
-    ),
-
-    // expression: $ => seq(
-    //   $._expression,
-    //   repeat(
-    //     $.filter
-    //   )
-    // ),
-
-    assignment: $ => seq(
-      "assign",
-      field('variable_name', $.identifier),
-      "=",
-      field('value', choice($.filter, $.expression)),
-    ),
-
-    _literal: $ => choice(
-      $.string,
-      $.number,
-      $.boolean,
-    ),
-
-    string: _ => choice(
-      seq("'", /[^']*/, "'"),
-      seq('"', /[^"]*/, '"'),
-    ),
-
-    number: _ => /\d+/,
-
-    boolean: _ => choice('true', 'false'),
-
-    identifier: _ => /([a-zA-Z_$][0-9a-zA-Z_]*)/,
-
-    argument_list: $ => seq(
-      choice($._literal, $.identifier, $.argument),
-      repeat(
-        seq(",",
-          choice($._literal, $.identifier, $.argument),
-        ),
+    filter: ($) =>
+      seq(
+        field("body", choice($.expression, $.filter)),
+        "|",
+        field("name", $.identifier),
+        optional(seq(":", $.argument_list))
       ),
-    ),
 
-    argument: $ => seq(
-      field('key', $.identifier),
-      ':',
-      field('value', choice($._literal, $.identifier)),
-    ),
+    statement: ($) => choice($.assignment),
 
-    predicate: $ => choice(
-      ...[
-        ['+', 'binary_plus'],
-        ['-', 'binary_plus'],
-        ['*', 'binary_times'],
-        ['/', 'binary_times'],
-        ['%', 'binary_times'],
-        ['^', 'binary_exp'],
-        ['==', 'binary_relation'],
-        ['<', 'binary_relation'],
-        ['<=', 'binary_relation'],
-        ['!=', 'binary_relation'],
-        ['>=', 'binary_relation'],
-        ['>', 'binary_relation'],
-        ['and', 'clause_connective'],
-        ['or', 'clause_connective'],
-      ].map(([operator, precedence]) =>
-        prec.left(precedence, seq(
-          field('left', $.expression),
-          field('operator', operator),
-          field('right', $.expression)
-        ))
-      )
-    ),
-  }
+    expression: ($) => choice($._literal, $.identifier, $.predicate),
+
+    assignment: ($) =>
+      seq(
+        "assign",
+        field("variable_name", $.identifier),
+        "=",
+        field("value", choice($.filter, $.expression))
+      ),
+
+    _literal: ($) => choice($.string, $.number, $.boolean),
+
+    string: (_) => choice(seq("'", /[^']*/, "'"), seq('"', /[^"]*/, '"')),
+
+    number: (_) => /\d+/,
+
+    boolean: (_) => choice("true", "false"),
+
+    identifier: (_) => /([a-zA-Z_$][0-9a-zA-Z_]*)/,
+
+    argument_list: ($) =>
+      seq(
+        choice($._literal, $.identifier, $.argument),
+        repeat(seq(",", choice($._literal, $.identifier, $.argument)))
+      ),
+
+    argument: ($) =>
+      seq(
+        field("key", $.identifier),
+        ":",
+        field("value", choice($._literal, $.identifier))
+      ),
+
+    predicate: ($) =>
+      choice(
+        ...[
+          ["+", "binary_plus"],
+          ["-", "binary_plus"],
+          ["*", "binary_times"],
+          ["/", "binary_times"],
+          ["%", "binary_times"],
+          ["^", "binary_exp"],
+          ["==", "binary_relation"],
+          ["<", "binary_relation"],
+          ["<=", "binary_relation"],
+          ["!=", "binary_relation"],
+          [">=", "binary_relation"],
+          [">", "binary_relation"],
+          ["and", "clause_connective"],
+          ["or", "clause_connective"],
+        ].map(([operator, precedence]) =>
+          prec.left(
+            precedence,
+            seq(
+              field("left", $.expression),
+              field("operator", operator),
+              field("right", $.expression)
+            )
+          )
+        )
+      ),
+  },
 });
