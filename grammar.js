2aea57ab0403ec676dd21103448ce0e0417e000c
Hank Jackson <hjackson277@gmail.com>
2024-02-02 12:07:13 -0500

    begin to fix precs with if/case

--- grammar.js
+++ grammar.js
@@ -1,7 +1,22 @@
+const PREC = {
+    primary: 1,
+    else_if: 1,
+    else: 2,
+}
+
+// refences
+// https://github.com/ngalaiko/tree-sitter-go-template/
+
 module.exports = grammar({
   name: "liquid",
 
   word: $ => $.identifier,
+
+  conflicts: $ => [
+    [$.else_tag],
+    [$.elseif_tag],
+    [$.when_tag]
+  ],
 
   precedences: (_) => [
     [
@@ -24,7 +39,6 @@ module.exports = grammar({
         $._node
       ),
 
-    //TODO: handle schemea tags inject json
     _node: ($) => 
       choice(
         $.tag,
@@ -33,12 +47,8 @@ module.exports = grammar({
 
     content: (_) => prec.right(repeat1(choice(/[^{]+|\{/, '{%%', '{{{'))),
 
-    tag: ($) => choice($._unpaired_tag, $._paired_tag),
+    tag: ($) => prec(2, choice($._unpaired_tag, $._paired_tag)),
 
-    //TODO: handle {{ and {% seperately
-    _tag_open: (_) => choice("{{", "{{-", "{%", "{%-"),
-
-    _tag_close: (_) => choice("}}", "-}}", "%}", "-%}"),
 
     //TODO:
     //raw tag
@@ -46,6 +56,7 @@ module.exports = grammar({
     //javascript tag
     //stylesheet tag
     //capture tag
+    //handle schemea tags inject json
     _paired_tag: ($) => 
       choice(
         $.if_tag,
@@ -98,7 +109,7 @@ module.exports = grammar({
       ),
 
 
-    //TODO: handle needed paired tags to statements for liquid tag
+    //TODO: handle needed paired tags -> statements for liquid tag
     _paired_statement: ($) =>
       choice(
         $.if_statement,
@@ -107,6 +118,167 @@ module.exports = grammar({
         $.for_loop_statement
       ),
 
+    /////////////////
+    // Paired Tags //
+    /////////////////
+
+    //TODO: cycle, pageinate, tablerow.. handle break & continue explicitly?
+    // https://shopify.dev/docs/api/liquid/tags/iteration-tags
+    //
+    // cleanup precs and test further, esp with mulit node else/ifelse blocks
+    for_loop_tag: ($) => 
+      prec(1,
+        seq(
+          seq(
+            $._tag_open,
+            "for",
+            field("item", $.identifier),
+            "in", 
+            choice(
+              seq(
+                field("iterator", choice($.identifier, $.access)), 
+                optional(field("modifier", choice($.argument_list, $.identifier))), 
+              ),
+              field("range", $.range)
+            ),
+            $._tag_close
+          ),
+          field("block", repeat($._node)),
+          optional($.else_tag),
+          $._tag_open, "endfor", $._tag_close,
+        )
+      ),
+
+    unless_tag: ($) =>
+      seq(
+        $._tag_open, "unless", field("condition", $.expression), $._tag_close,
+        field("consequence", alias(repeat($._node), $.block)),
+        $._tag_open, "endunless", $._tag_close, 
+      ),
+
+    if_tag: ($) =>
+      seq(
+        $._tag_open, "if", field("condition", $.expression), $._tag_close, 
+        repeat($._node),
+        repeat($.elseif_tag),
+        optional($.else_tag),
+        prec.right(0, seq($._tag_open, "endif", $._tag_close)), 
+      ),
+
+    elseif_tag: ($) =>
+      prec.dynamic(
+        PREC.else_if,
+        seq(
+          $._tag_open, "elseif", field("condition", $.expression), $._tag_close,
+          alias(repeat($._node), $.block),
+        ),
+      ),
+
+    else_tag: ($) =>
+      prec.dynamic(
+        PREC.else,
+        seq(
+          $._tag_open, "else", $._tag_close,
+          alias(repeat($._node), $.block),
+      )),
+
+    when_tag: ($) => 
+      prec.dynamic(
+        PREC.else_if,
+        seq(
+          //condtion should be more constrained -- https://shopify.dev/docs/api/liquid/tags/case
+          $._tag_open, "when", field("condition", $.expression), $._tag_close,
+          field("consequence", alias(repeat($._node), $.block)),
+        ),
+      ),
+
+    case_tag: ($) =>
+      seq(
+        $._tag_open, "case", field("receiver", choice($.identifier, $.access)), $._tag_close,
+        repeat(field("consequence", $.when_tag)),
+        optional(field("alternative", $.else_tag)),
+        prec.right(0, seq($._tag_open, "endcase", $._tag_close)),
+      ),
+
+
+    //////////////////////////////////////
+    // Paired Statements For Liquid Tag //
+    //////////////////////////////////////
+    //
+    //TODO: test / handle multi node if/case and set precs, re: paired tags
+    
+    for_loop_statement: ($) => 
+      prec(1,
+        seq(
+          seq(
+            "for",
+            field("item", $.identifier),
+            "in", 
+            choice(
+              seq(
+                field("iterator", choice($.identifier, $.access)), 
+                optional(field("modifier", choice($.argument_list, $.identifier))), 
+              ),
+              field("range", $.range)
+            ),
+          ),
+          field("block", $._liquid_block),
+          optional($.else_statement),
+          "endfor"
+        )
+      ),
+
+    unless_statement: ($) =>
+      seq(
+        seq("unless", field("condition", $.expression)),
+        field("consequence", alias($._liquid_block, $.block)),
+        "endunless"
+      ),
+
+    if_statement: ($) =>
+      seq(
+        "if", field("condition", $.expression),
+        field("consequence", alias($._liquid_block, $.block)),
+        repeat(field("alternatives", $.elseif_statement)),
+        optional(field("alternatives", $.else_statement)),
+        "endif"
+      ),
+
+    elseif_statement: ($) =>
+      prec.left(
+        1,
+        seq(
+          "elseif", field("condition", $.expression),
+          field("consequence", alias($._liquid_block, $.block)),
+        ),
+      ),
+
+    else_statement: ($) =>
+      prec.left(
+        1,
+        seq(
+          "else",
+          field("consequence", alias($._liquid_block, $.block))
+        ),
+      ),
+
+    when_statement: ($) => 
+      prec.left(
+        1,
+        seq(
+          //condtion should be more constrained -- https://shopify.dev/docs/api/liquid/tags/case
+          "when", field("condition", $.expression),
+          field("consequence", alias($._liquid_block, $.block)),
+        ),
+      ),
+
+    case_statment: ($) =>
+      seq(
+        "case", field("receiver", choice($.identifier, $.access)),
+        repeat1(field("consequence", $.when_statement)),
+        optional(field("alternative", $.else_statement)),
+        "endcase", 
+      ),
 
     //should probably just explicitly define these lol
     ...[
@@ -255,165 +427,6 @@ module.exports = grammar({
       ),
 
 
-    /////////////////
-    // Paired Tags //
-    /////////////////
-
-    //TODO: cycle, pageinate, tablerow.. handle break & continue explicitly?
-    // https://shopify.dev/docs/api/liquid/tags/iteration-tags
-    for_loop_tag: ($) => 
-      prec(1,
-        seq(
-          seq(
-            $._tag_open,
-            "for",
-            field("item", $.identifier),
-            "in", 
-            choice(
-              seq(
-                field("iterator", choice($.identifier, $.access)), 
-                optional(field("modifier", choice($.argument_list, $.identifier))), 
-              ),
-              field("range", $.range)
-            ),
-            $._tag_close
-          ),
-          field("block", repeat($._node)),
-          optional($.else_tag),
-          $._tag_open, "endfor", $._tag_close,
-        )
-      ),
-
-    unless_tag: ($) =>
-      seq(
-        $._tag_open, "unless", field("condition", $.expression), $._tag_close,
-        field("consequence", alias(repeat($._node), $.block)),
-        $._tag_open, "endunless", $._tag_close, 
-      ),
-
-    if_tag: ($) =>
-      seq(
-        $._tag_open, "if", field("condition", $.expression), $._tag_close, 
-        field("consequence", alias(repeat1($._node), $.block)),
-        repeat(field("alternatives", $.elseif_tag)),
-        optional(field("alternatives", $.else_tag)),
-        $._tag_open, "endif", $._tag_close, 
-      ),
-
-    elseif_tag: ($) =>
-      prec.left(
-        1,
-        seq(
-          $._tag_open, "elseif", field("condition", $.expression), $._tag_close,
-          field("consequence", alias(repeat1($._node), $.block)),
-        ),
-      ),
-
-    else_tag: ($) =>
-      prec.left(
-        1,
-        seq(
-          $._tag_open, "else", $._tag_close,
-          field("consequence", alias(repeat1($._node), $.block))
-        ),
-      ),
-
-    when_tag: ($) => 
-      prec.left(
-        1,
-        seq(
-          //condtion should be more constrained -- https://shopify.dev/docs/api/liquid/tags/case
-          $._tag_open, "when", field("condition", $.expression), $._tag_close,
-          field("consequence", alias(repeat1($._node), $.block)),
-        ),
-      ),
-
-    case_tag: ($) =>
-      seq(
-        $._tag_open, "case", field("receiver", choice($.identifier, $.access)), $._tag_close,
-        repeat(field("consequence", $.when_tag)),
-        optional(field("alternative", $.else_tag)),
-        $._tag_open, "endcase", $._tag_close, 
-      ),
-
-
-    //////////////////////////////////////
-    // Paired Statements For Liquid Tag //
-    //////////////////////////////////////
-    for_loop_statement: ($) => 
-      prec(1,
-        seq(
-          seq(
-            "for",
-            field("item", $.identifier),
-            "in", 
-            choice(
-              seq(
-                field("iterator", choice($.identifier, $.access)), 
-                optional(field("modifier", choice($.argument_list, $.identifier))), 
-              ),
-              field("range", $.range)
-            ),
-          ),
-          field("block", $._liquid_block),
-          optional($.else_statement),
-          "endfor"
-        )
-      ),
-
-    unless_statement: ($) =>
-      seq(
-        seq("unless", field("condition", $.expression)),
-        field("consequence", alias($._liquid_block, $.block)),
-        "endunless"
-      ),
-
-    if_statement: ($) =>
-      seq(
-        "if", field("condition", $.expression),
-        field("consequence", alias($._liquid_block, $.block)),
-        repeat(field("alternatives", $.elseif_statement)),
-        optional(field("alternatives", $.else_statement)),
-        "endif"
-      ),
-
-    elseif_statement: ($) =>
-      prec.left(
-        1,
-        seq(
-          "elseif", field("condition", $.expression),
-          field("consequence", alias($._liquid_block, $.block)),
-        ),
-      ),
-
-    else_statement: ($) =>
-      prec.left(
-        1,
-        seq(
-          "else",
-          field("consequence", alias($._liquid_block, $.block))
-        ),
-      ),
-
-    when_statement: ($) => 
-      prec.left(
-        1,
-        seq(
-          //condtion should be more constrained -- https://shopify.dev/docs/api/liquid/tags/case
-          "when", field("condition", $.expression),
-          field("consequence", alias($._liquid_block, $.block)),
-        ),
-      ),
-
-    case_statment: ($) =>
-      seq(
-        "case", field("receiver", choice($.identifier, $.access)),
-        repeat1(field("consequence", $.when_statement)),
-        optional(field("alternative", $.else_statement)),
-        "endcase", 
-      ),
-
-
     //////////////
     // Comments //
     //////////////
@@ -428,6 +441,12 @@ module.exports = grammar({
         //hacky way of dealing with {% terminating sequence 
         "endcomment", alias($._tag_close, ""), 
       ),
+
+
+    // TODO: distinguish between "{{" and "{%" delmiters
+    _tag_open: (_) => choice("{{", "{{-", "{%", "{%-"),
+
+    _tag_close: (_) => choice("}}", "-}}", "%}", "-%}"),
   },
 
 });
